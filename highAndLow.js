8e3e5e60a743abcb1fa657ac9bb5ea74b8589b7f
VladimirAvdeev <sofist2012@gmail.com>
2022-09-08 17:58:55 +0300

    feat: finish first task

---
+++ highAndLow.js
@@ -0,0 +1,10 @@
+function highAndLow(numbers){
+    let arrStr = numbers.split(' '); // delete spaces
+    let result;
+    arrStr.sort(function(a,b){  //descending sort with compare function
+        return b - a;
+    });
+    result = (arrStr[0] + ' ' + arrStr[arrStr.length-1]);  
+    return result;
+}
+  console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
