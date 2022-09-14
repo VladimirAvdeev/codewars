aa891b461f9bc1d27568cff036acec2312b913f5
VladimirAvdeev <sofist2012@gmail.com>
2022-09-14 21:22:47 +0300

    feat: finish 13th task

--- HumanReadableDurationFormat.js
+++ HumanReadableDurationFormat.js
@@ -4,41 +4,67 @@ function formatDuration (seconds) {
     const hour = 3600;
     const min = 60;
     let result = '';
+    let resultArr;
     recurse(seconds);
     function recurse (sec) {
         if(sec >= year){
             if(sec%year){
                 result += 
-                Math.round(sec / year) > 1 
-                ? Math.round(sec / year) + ' years ' 
-                : Math.round(sec / year) + ' year '; 
+                Math.floor(sec / year) > 1 
+                ? Math.floor(sec / year) + ' years ' 
+                : Math.floor(sec / year) + ' year '; 
                 recurse(sec%year);
             }else result += 
-            Math.round(sec / year) > 1 
-            ? Math.round(sec / year) + ' years ' 
-            : Math.round(sec / year) + ' year ';   
+            Math.floor(sec / year) > 1 
+            ? Math.floor(sec / year) + ' years ' 
+            : Math.floor(sec / year) + ' year ';   
         }else if(sec >= day){
             if(sec%day){
-                result += (Math.round(sec / day)).toString() + ' days '; 
+                result += Math.floor(sec / day) > 1 
+                ? Math.floor(sec / day) + ' days ' 
+                : Math.floor(sec / day) + ' day ';   
                 recurse(sec%day);
-            }else result += (Math.round(sec / day)).toString() + ' days ';
+            }else result += Math.floor(sec / day) > 1 
+            ? Math.floor(sec / day) + ' days ' 
+            : Math.floor(sec / day) + ' day ';  
         }else if(sec >= hour){
             if(sec%hour){
-                result += (Math.round(sec / hour)).toString() + ' hours '; 
+                result += Math.floor(sec / hour) > 1 
+                ? Math.floor(sec / hour) + ' hours ' 
+                : Math.floor(sec / hour) + ' hour ';  
                 recurse(sec%hour);
-            }else result += (Math.round(sec / hour)).toString() + ' hours ';
+            }else result += Math.floor(sec / hour) > 1 
+            ? Math.floor(sec / hour) + ' hours ' 
+            : Math.floor(sec / hour) + ' hour ';  
         }else if(sec >= min){
             if(sec%min){
-                result += (Math.round(sec / min)).toString() + ' minutes '; 
+                result += Math.floor(sec / min) > 1 
+                ? Math.floor(sec / min) + ' minutes ' 
+                : Math.floor(sec / min) + ' minute ';   
                 recurse(sec%min);
-            }else result += (Math.round(sec / min)).toString() + ' minutes '; 
-        }else result += (Math.round(sec / 1)).toString() + ' seconds';
+            }else result += Math.floor(sec / min) > 1 
+            ? Math.floor(sec / min) + ' minutes ' 
+            : Math.floor(sec / min) + ' minute ';
+        }else result += Math.floor(sec / 1) > 1 
+        ? Math.floor(sec / 1) + ' seconds' 
+        : Math.floor(sec / 1) + ' second';
     }
-    console.log(result.split(' '));
+    resultArr = result.trim().split(' ');
+    if(resultArr.length > 2) {
+        resultArr.splice((resultArr.length-2),0,'and');
+        for (let i = 0; i < resultArr.indexOf('and'); i++){
+            if(i%2 && resultArr[i+1] != 'and'){
+                resultArr[i]+=',';
+            }
+            
+        }
+        return resultArr.join(' ');
+    }else if (resultArr[0] ==='0'){
+        return 'now';
+    }else return resultArr.join(' ');
     
 }
-
-console.log(formatDuration(31536002));
+console.log(formatDuration(0));
 
 //1 min = 60 sec
 //1 hour = 3 600 sec
