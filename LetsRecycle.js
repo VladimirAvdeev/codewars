e9c31b7fd7b5bccb612c6a638e6a643c4a96d3db
VladimirAvdeev <sofist2012@gmail.com>
2022-09-15 01:00:14 +0300

    refactor: rename 16th task js file

---
+++ LetsRecycle.js
@@ -0,0 +1,12 @@
+function recycle(array) {
+    let result = [[],[],[],[]];
+    console.log(result);
+}
+
+console.log(recycle([
+    {type: 'rotten apples', material: 'organic'},
+    {type: 'out of date yogurt', material: 'organic', secondMaterial: 'plastic'},
+    {type: 'wine bottle', material: 'glass', secondMaterial: 'paper'},
+    {type: 'amazon box', material: 'paper'},
+    {type: 'beer bottle', material: 'glass', secondMaterial: 'paper'}
+  ]));
