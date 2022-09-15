e59fa52204a3293a0bcb51a8725a0b7750ea403a
VladimirAvdeev <sofist2012@gmail.com>
2022-09-15 19:06:28 +0300

    feat: finish 18th task

--- JavascriptFromTheInside1Map.js
+++ JavascriptFromTheInside1Map.js
@@ -1,6 +1,15 @@
-base_array = [1, 2, 3, 4, 5];
-Array.prototype.map=function(func){
-    
+Array.prototype.map = function (func, thisArg = this) {
+    let result = new Array(this.length);
+    let array = this;
+    for (let i = 0; i < array.length; i++) {
+        if (i in array) result[i] = (func.call(thisArg, array[i], i, array));
+    }
+    return result;
 }
-
-console.log(base_array.map(function(x) { return x; }));
+//console.log([, ,].map(function (x) { return 1; }));
+//console.log(new Array(2).map(function (x) { return 1; }));
+console.log([NaN, 2, 3, 4, 5].map(function (x) { return x; }));
+console.log(new Array(2).concat([null]).map(function (x) { return 1; }));
+console.log(new Array(2).concat([null]).map(function (x) { return 1; }));
+console.log(null != undefined);
+console.log(NaN !== undefined);
