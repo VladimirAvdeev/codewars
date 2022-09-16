c143eeaddb4142ba8d63f018d49d4ce1624e4360
VladimirAvdeev <sofist2012@gmail.com>
2022-09-16 22:43:15 +0300

    feat: finish 19th task

--- JavascriptFromTheInside2Filter.js
+++ JavascriptFromTheInside2Filter.js
@@ -1,20 +1,27 @@
 base_array = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5];
+arr['00'] = 41;
+arr['01'] = 42;
 Array.prototype.filter = function (func, thisArg = this) {
-    let result = new Array(this.length);
+    let result = [];
     let array = this;
-    for (let i = 0; i < array.length; i++) {
+    let arrayLen = this.length;
+    for (let i = 0; i < arrayLen; i++) {
         if (i in array) {
-            if ((func.call(thisArg, array[i], i, array))) {
-                result[i] = array[i];
-            } else {
-                //result.push(array[i]);
+            if (!!(func.call(thisArg, array[i], i, array))) {
+                result.push(array[i]);
             }
         }
     }
-    result.length = 1
     return result;
 }
 
 //console.log(base_array.filter((x) => x < 5));
-console.log(base_array.filter((x) => x == 1));
-//console.log(base_array.filter((x) => x % 2 != 0));
+//console.log(base_array.filter((x) => x == 1));
+//console.table(base_array.filter((_, i, orig) => orig[i - 1]));
+//console.log(base_array.filter((_, i, orig) => orig.filter((__, ii) => (ii % i) < 2).length == 0));
+//console.log(arr.filter(x => true));
+let filtered_array = arr.filter(x => true);
+console.log(Object.keys(filtered_array), [0, 1, 2, 3, 4].map(String));
+console.log(+'00');
+console.log(parseFloat('00'));
